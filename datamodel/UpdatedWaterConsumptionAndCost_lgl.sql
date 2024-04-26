e0fb87729a4b00d25d5f68f81d07f2d6726ee45d
Sara Wexler <sarawexler2@gmaill.com>
2024-04-26 17:36:52 -0500

    updating data model

--- datamodel/UpdatedWaterConsumptionAndCost_lgl.sql
+++ datamodel/UpdatedWaterConsumptionAndCost_lgl.sql
@@ -1,12 +1,16 @@
 CREATE SCHEMA IF NOT EXISTS "WaterConsumptionAndCost";
 
-CREATE  TABLE "WaterConsumptionAndCost".dim_billingcycle ( 
-	billingcycleid       integer  NOT NULL  ,
-	revenuemonth         varchar(250)    ,
-	servicestartdate     date    ,
-	serviceenddate       date    ,
-	numberofdays         integer    ,
-	CONSTRAINT pk_dim_billingcycle PRIMARY KEY ( billingcycleid )
+CREATE  TABLE "WaterConsumptionAndCost".dim_date ( 
+	dateid               integer  NOT NULL  ,
+	"date"               date    ,
+	yearnumber           integer    ,
+	quarternumber        integer    ,
+	monthnumber          integer    ,
+	daynumber            integer    ,
+	monthname            varchar(350)    ,
+	dayname              varchar(350)    ,
+	weekofyear           integer    ,
+	CONSTRAINT pk_dim_billingcycle PRIMARY KEY ( dateid )
  );
 
 CREATE  TABLE "WaterConsumptionAndCost".dim_development ( 
@@ -22,9 +26,6 @@ CREATE  TABLE "WaterConsumptionAndCost".dim_development (
 CREATE  TABLE "WaterConsumptionAndCost".dim_location ( 
 	locationid           integer  NOT NULL  ,
 	boroughname          varchar(250)    ,
-	"zone"               varchar(250)    ,
-	longitude            numeric(7,2)    ,
-	latitude             numeric(7,2)    ,
 	CONSTRAINT pk_dim_location PRIMARY KEY ( locationid )
  );
 
@@ -34,6 +35,29 @@ CREATE  TABLE "WaterConsumptionAndCost".dim_meter (
 	meterscope           varchar(250)    ,
 	CONSTRAINT pk_dim_meter PRIMARY KEY ( meternumber )
  );
+
+CREATE  TABLE "WaterConsumptionAndCost".dim_vendor ( 
+	vendorid             integer  NOT NULL  ,
+	vendorname           varchar(250)    ,
+	CONSTRAINT pk_dim_vendor PRIMARY KEY ( vendorid )
+ );
+
+CREATE  TABLE "WaterConsumptionAndCost".facts_consumerwaterusage ( 
+	factid               integer  NOT NULL  ,
+	vendorid             integer  NOT NULL  ,
+	locationid           integer  NOT NULL  ,
+	servicestartdateid   date  NOT NULL  ,
+	serviceenddateid     date  NOT NULL  ,
+	developmentid        integer  NOT NULL  ,
+	meternumber          integer  NOT NULL  ,
+	consumptionvolume    numeric(7,2)    ,
+	currentcharges       numeric(7,2)    ,
+	waterandsewercharges numeric(7,2)    ,
+	othercharges         numeric(7,2)    ,
+	serviceduration      numeric(7,2)    ,
+	CONSTRAINT pk_facts_consumerwaterusage PRIMARY KEY ( factid )
+ );
+
 
 CREATE  TABLE "WaterConsumptionAndCost".dim_vendor ( 
 	vendorid             integer  NOT NULL  ,
