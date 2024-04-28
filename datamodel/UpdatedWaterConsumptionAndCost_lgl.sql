f6ba14fb41cca29327e222d37b83c0e7798bc64c
Sara Wexler <sarawexler2@gmaill.com>
2024-04-28 01:17:02 -0500

    Updating Model

--- datamodel/UpdatedWaterConsumptionAndCost_lgl.sql
+++ datamodel/UpdatedWaterConsumptionAndCost_lgl.sql
@@ -30,10 +30,11 @@ CREATE  TABLE "WaterConsumptionAndCost".dim_location (
  );
 
 CREATE  TABLE "WaterConsumptionAndCost".dim_meter ( 
-	meternumber          varchar  NOT NULL  ,
+	meterid              varchar  NOT NULL  ,
 	meteramr             boolean    ,
 	meterscope           varchar(250)    ,
-	CONSTRAINT pk_dim_meter PRIMARY KEY ( meternumber )
+	meternumber          varchar    ,
+	CONSTRAINT pk_dim_meter PRIMARY KEY ( meterid )
  );
 
 CREATE  TABLE "WaterConsumptionAndCost".dim_vendor ( 
@@ -49,37 +50,11 @@ CREATE  TABLE "WaterConsumptionAndCost".facts_consumerwaterusage (
 	servicestartdateid   date  NOT NULL  ,
 	serviceenddateid     date  NOT NULL  ,
 	developmentid        integer  NOT NULL  ,
-	meternumber          integer  NOT NULL  ,
+	meterid              varchar  NOT NULL  ,
 	consumptionvolume    numeric(7,2)    ,
 	currentcharges       numeric(7,2)    ,
 	waterandsewercharges numeric(7,2)    ,
 	othercharges         numeric(7,2)    ,
 	serviceduration      numeric(7,2)    ,
-	CONSTRAINT pk_facts_consumerwaterusage PRIMARY KEY ( factid )
- );
-
-
-CREATE  TABLE "WaterConsumptionAndCost".dim_vendor ( 
-	vendorid             integer  NOT NULL  ,
-	vendorname           varchar(250)    ,
-	CONSTRAINT pk_dim_vendor PRIMARY KEY ( vendorid )
- );
-
-CREATE  TABLE "WaterConsumptionAndCost".facts_consumerwaterusage ( 
-	factid               integer  NOT NULL  ,
-	billingcycleid       integer  NOT NULL  ,
-	locationid           integer  NOT NULL  ,
-	vendorid             integer  NOT NULL  ,
-	developmentid        integer  NOT NULL  ,
-	meternumber          integer  NOT NULL  ,
-	consumptionvolume    numeric(7,2)    ,
-	waterconsumptioncost numeric(7,2)    ,
-	totalutilitycost     numeric(7,2)    ,
-	currentcharges       numeric(7,2)    ,
-	watercharges         numeric(7,2)    ,
-	sewercharges         numeric(7,2)    ,
-	othercharges         numeric(7,2)    ,
-	electricitycost      numeric(7,2)    ,
-	gascost              numeric(7,2)    ,
 	CONSTRAINT pk_facts_consumerwaterusage PRIMARY KEY ( factid )
  );
